fc5cd07e9fd0e3a278230d8f0d88f3f251db5eaa
Kenrick-MH <kenrickmarcellhaditio@gmail.com>
2023-11-04 14:36:59 -0700

    Task4

--- shifter.sv
+++ shifter.sv
@@ -1,16 +1,16 @@
 module shifter(in,shift,sout);
-input [15:0] in;
-input [1:0] shift;
-output [15:0] sout;
+  input [15:0] in;
+  input [1:0] shift;
+  output [15:0] sout;
 
-  always @(shift) begin
-    case (shift)
-      2'b00: sout = in;
-      2'b01: sout = {in[14:0], 1'b0};
-      2'b10: sout = {1'b0, in[15:1]};
-      2'b11: sout = {in[15:15], in[15:1]};
-    endcase
-  end
+    always @(shift) begin
+      case (shift)
+        2'b00: sout = in;
+        2'b01: sout = in << 1;
+        2'b10: sout = in >> 1;
+        2'b11: sout = {in[15], (in >> 1)[14:0]};
+      endcase
+    end
 
 endmodule
 
