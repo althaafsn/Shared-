33f429bc602708d0762141e7037126ab5e9f79af
StiZro <124484925+StiZro@users.noreply.github.com>
2023-11-19 00:17:02 -0800

    Finished datapath

--- datapath.sv
+++ datapath.sv
@@ -4,6 +4,11 @@
 `define VSEL_MDATA 2'b11
 
 
+/*
+    changed vsel, now supports immediate values
+    changed status
+*/
+
 module datapath (
     // clock
     clk,
@@ -14,19 +19,19 @@ module datapath (
     // immideate
     imm5,
     // set register
-    writenum, write, mdata, sximm8, pc, c,
+    writenum, write, mdata, sximm8, pc,
     // output
-    status_out, datapath_out );
+    status_out, c);
 
     input clk, loada, loadb, loadc, loads, write, asel, bsel;
     input [1:0] ALUop, shift, vsel; 
     input [2:0] readnum, writenum;
-    input [15:0] mdata, sximm8, pc, c;
+    input [15:0] mdata, sximm8, pc;
     input [4:0] imm5;
     output reg [2:0] status_out;
-    output reg [15:0] datapath_out;
+    output reg [15:0] c;
 
-    wire sximm5 = {{11{imm5[4]}}, imm5};
+    wire [15:0] sximm5 = {{11{imm5[4]}}, imm5};
     reg [15:0] data_in_reg, data_out_reg, A_FF, B_FF, s_out, A_in_ALU, B_in_ALU, out_ALU;
     reg [2:0] status; 
 
@@ -34,10 +39,9 @@ module datapath (
     regfile REGFILE(data_in_reg, writenum, write, readnum, clk, data_out_reg);
     ALU ALUModule(A_in_ALU, B_in_ALU, ALUop, out_ALU, status);
     
-    
     vDFF #(16) A_loader(clk & loada, data_out_reg, A_FF);
     vDFF #(16) B_loader(clk & loadb, data_out_reg, B_FF);
-    vDFF #(16) C_loader(clk & loadc, out_ALU, datapath_out);
+    vDFF #(16) C_loader(clk & loadc, out_ALU, c);
     vDFF #(3) status_loader(clk & loads, status, status_out);
     
     //Datapath in
@@ -53,25 +57,20 @@ module datapath (
         endcase
     end
 
+    // ASEL and BSEL
+    always_comb begin
 
-    // always_comb begin
-    //   if (vsel == 2'b01) begin
-    //     data_in_reg <= datapath_in;
-    //   end else begin
-    //     data_in_reg <= datapath_out;
-    //   end
+      if (asel == 1'b1) begin
+        A_in_ALU <= 16'b0;
+      end else begin
+        A_in_ALU <= A_FF;
+      end
 
-    //   if (asel == 1'b1) begin
-    //     A_in_ALU <= 16'b0;
-    //   end else begin
-    //     A_in_ALU <= A_FF;
-    //   end
-
-    //   if (bsel == 1'b1) begin
-    //     B_in_ALU <= sximm5;
-    //   end else begin
-    //     B_in_ALU <= s_out;
-    //   end
-    // end
+      if (bsel == 1'b1) begin
+        B_in_ALU <= sximm5;
+      end else begin
+        B_in_ALU <= s_out;
+      end
+    end
 
 endmodule
--- datapath_tb.sv
+++ datapath_tb.sv
@@ -8,6 +8,14 @@
 `define AND 2'b10
 `define MVN 2'b11
 
+// VSEL 
+`define VSEL_C 2'b00
+`define VSEL_PC 2'b01
+`define VSEL_IMM 2'b10
+`define VSEL_MDATA 2'b11
+
+// REGISTERS
+
 `define r0 0
 `define r1 1
 `define r2 2
@@ -17,15 +25,47 @@
 `define r6 6
 `define r7 7
 
+
+    // input clk, loada, loadb, loadc, loads, write, asel, bsel;
+    // input [1:0] ALUop, shift, vsel; 
+    // input [2:0] readnum, writenum;
+    // input [15:0] mdata, sximm8, pc, c;
+    // input [4:0] imm5;
+    // output reg [2:0] status_out;
+    // output reg [15:0] datapath_out;
+
+
 module datapath_tb();
-  reg clk, sim_vsel, sim_loada, sim_loadb, sim_loadc, sim_loads, sim_write, sim_asel, sim_bsel;
-  reg [1:0] sim_ALUop, sim_shift; 
-  reg [2:0] sim_readnum, sim_writenum;
-  reg err;
+  reg clk, sim_loada, sim_loadb, sim_loadc, sim_loads, sim_write, sim_asel, sim_bsel, err;
+  reg [1:0] sim_ALUop, sim_shift, sim_vsel; 
+  reg [2:0] sim_readnum, sim_writenum, sim_status_out;
+  reg [15:0] sim_sximm8, sim_pc, sim_mdata;
+  reg [4:0] sim_imm5;
 
   // output
-  reg signed [15:0] sim_datapath_in, sim_datapath_out;
-  wire signed   [15:0] allRegs [7:0];
+  reg signed [15:0] sim_c;
+  wire signed [15:0] allRegs [7:0];
+
+  datapath DUT (
+                  .clk(clk),
+                  .readnum(sim_readnum),
+                  .vsel(sim_vsel),
+                  .loada(sim_loada),
+                  .loadb(sim_loadb),
+                  .shift(sim_shift),
+                  .asel(sim_asel),
+                  .bsel(sim_bsel),
+                  .ALUop(sim_ALUop),
+                  .loadc(sim_loadc),
+                  .loads(sim_loads),
+                  .imm5(sim_imm5),
+                  .writenum(sim_writenum),
+                  .write(sim_write),
+                  .mdata(sim_mdata),
+                  .sximm8(sim_sximm8),
+                  .pc(sim_pc),
+                  .status_out(sim_status_out),
+                  .c(sim_c));
   
 assign allRegs = {DUT.REGFILE.R7,
                 DUT.REGFILE.R6, 
@@ -35,7 +75,6 @@ assign allRegs = {DUT.REGFILE.R7,
                 DUT.REGFILE.R2, 
                 DUT.REGFILE.R1, 
                 DUT.REGFILE.R0};  
-  reg sim_Z_out;
 
   // wire [15:0] allRegs [7:0];  
 
@@ -46,19 +85,23 @@ assign allRegs = {DUT.REGFILE.R7,
   // MOV for immediate values
   task MOV;
     input [2:0] Dest;
-    input [15:0] WriteVal;
+    input [7:0] WriteVal;
+    input [15:0] expectedWrite;
+    input [1:0] changeVsel;
 
     // Sets write mode to 1
     sim_write = 1'b1;
-    sim_vsel = 1'b1;
-    sim_datapath_in = WriteVal;
+    sim_vsel = changeVsel;
+
+    // Sign extension
+    sim_sximm8 = {{8{WriteVal[7]}}, WriteVal};
     sim_writenum = Dest;
 
     #4; // after rising edge
 
-    assert (allRegs[Dest] == WriteVal) $display ("Overwrite to R%d sucessful!", Dest);
+    assert (allRegs[Dest] == expectedWrite) $display ("Overwrite to R%d sucessful!", Dest);
     else begin 
-      $display ("Overwrite to R%d failed, got %d instead of %d", Dest, allRegs[Dest], WriteVal);
+      $display ("Overwrite to R%d failed, got %d instead of %d", Dest, allRegs[Dest], expectedWrite);
       err = 1'b1;
     end
 
@@ -82,7 +125,7 @@ assign allRegs = {DUT.REGFILE.R7,
     input [2:0] Bin;
     input [1:0] shift;
     input signed [15:0] expectedVal;
-    input expectedZ;
+    input [2:0] expectedStatus;
     
     // Initialization
     sim_ALUop = OP;
@@ -94,7 +137,7 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_loads = 0;
     sim_asel = 0;
     sim_bsel = 0;
-    sim_vsel = 0;
+    sim_vsel = `VSEL_C;
     sim_shift = shift;
 
     // Read and load from Ain
@@ -121,30 +164,21 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_loads = 1'b0;
     
     // Now write to Dest
-    assert (sim_datapath_out == expectedVal) $display ("ADD sucesssful");
+    assert (sim_c == expectedVal) $display ("ADD sucesssful");
     else begin 
-      $display("ADD unsuccessful, got %d but expected %d", sim_datapath_out, expectedVal);
+      $display("ADD unsuccessful, got %d but expected %d", sim_c, expectedVal);
       err = 1'b1;
     end
 
-    // Check Z value
-    if (sim_datapath_out == 0) begin
-      assert (sim_Z_out == 1'b1) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
+    // CHECK STATUS
+    assert (sim_status_out == expectedStatus) $display("Status correct");
+    else begin 
+        $display("Status incorrect, got %b but expected %b", sim_status_out, expectedStatus);
         err = 1'b1;
-      end
-    end else begin
-      assert (sim_Z_out == 1'b0) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end 
+    end
     
-    MOV(Dest, sim_datapath_out);
+    MOV(Dest, sim_c, sim_c,`VSEL_C);
     #4;
-
   endtask
 
   task OP_I;
@@ -154,7 +188,7 @@ assign allRegs = {DUT.REGFILE.R7,
     input [4:0] IMM;
     input [1:0] shift;
     input signed [15:0] expectedVal;
-    input expectedZ;
+    input [2:0] expectedStatus;
     
     sim_write = 1'b0;
     sim_loada = 0;
@@ -167,7 +201,7 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_asel = 0;
     sim_bsel = 1;
 
-    sim_vsel = 0;
+    sim_vsel = `VSEL_IMM;
     sim_shift = shift;
 
     sim_writenum = Dest;
@@ -181,7 +215,7 @@ assign allRegs = {DUT.REGFILE.R7,
 
     // Set value of B
     sim_loada = 0;
-    sim_datapath_in = {{11{1'b0}}, IMM};
+    sim_imm5 = IMM;
     #4
 
     // Caclulates A + B, load to C and S
@@ -193,28 +227,20 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_loads = 1'b0;
     
     // Now write to Dest
-    assert (sim_datapath_out == expectedVal) $display ("Operation %b sucesssful", OP);
+    assert (sim_c == expectedVal) $display ("Operation %b sucesssful", OP);
     else begin 
-      $display("Operation %b unsuccessful, got %d but expected %d", OP, sim_datapath_out, expectedVal);
+      $display("Operation %b unsuccessful, got %d but expected %d", OP, sim_c, expectedVal);
       err = 1'b1;
     end
 
-    // Check Z value
-    if (sim_datapath_out == 0) begin
-      assert (sim_Z_out == 1'b1) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end else begin
-      assert (sim_Z_out == 1'b0) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end 
+    // Check status
+    assert (sim_status_out == expectedStatus) $display ("STATUS correct");
+    else begin 
+      $display("Status incorrect, got %b but expected %b", sim_status_out, expectedStatus);
+      err = 1'b1;
+    end
     
-    MOV(Dest, sim_datapath_out);
+    MOV(Dest, sim_c, sim_c, `VSEL_C);
     #4;
   
   endtask
@@ -224,7 +250,7 @@ assign allRegs = {DUT.REGFILE.R7,
     input [2:0] Dest;
     input [2:0] Reg;
     input signed [15:0] expectedVal;
-    input expectedZ;
+    input expectedStatus;
     
     // Initialization
     sim_ALUop = `ADD;
@@ -238,7 +264,7 @@ assign allRegs = {DUT.REGFILE.R7,
     // Set a to 0 (one operand)
     sim_asel = 1;
     sim_bsel = 0;
-    sim_vsel = 0;
+    sim_vsel = `VSEL_C;
     sim_shift = shift;
 
     // Read from Reg 
@@ -257,28 +283,20 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_loads = 1'b0;
     
     // Now write to Dest
-    assert (sim_datapath_out == expectedVal) $display ("Shift is corrrect");
+    assert (sim_c == expectedVal) $display ("Shift is corrrect");
     else begin 
-      $display("Shift unsuccessful, got %b but expected %b", sim_datapath_out, expectedVal);
+      $display("Shift unsuccessful, got %b but expected %b", sim_c, expectedVal);
       err = 1'b1;
     end
 
-    // Check Z value
-    if (sim_datapath_out == 0) begin
-      assert (sim_Z_out == 1'b1) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end else begin
-      assert (sim_Z_out == 1'b0) $display ("ADD sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end 
-    
-    MOV(Dest, sim_datapath_out);
+    // Check status value
+    assert (sim_status_out == expectedStatus) $display ("SHIFT sucesssful");
+    else begin 
+      $display("Status incorrect, got %b but expected %b", sim_status_out, expectedStatus);
+      err = 1'b1;
+    end
+
+    MOV(Dest, sim_c, sim_c, `VSEL_C);
     #4;
 
   endtask
@@ -288,7 +306,7 @@ assign allRegs = {DUT.REGFILE.R7,
     input [2:0] Reg;
     input [1:0] shift;
     input [15:0] expectedVal;
-    input expectedZ;
+    input expectedStatus;
     
     // Initialization
     sim_ALUop = `MVN;
@@ -319,56 +337,23 @@ assign allRegs = {DUT.REGFILE.R7,
     sim_loads = 1'b0;
     
     // Now write to Dest
-    assert (sim_datapath_out == expectedVal) $display ("MVN output OK");
+    assert (sim_c == expectedVal) $display ("MVN output OK");
     else begin 
-      $display("MVN unsuccessful, got %b but expected %b", sim_datapath_out, expectedVal);
+      $display("MVN unsuccessful, got %b but expected %b", sim_c, expectedVal);
       err = 1'b1;
     end
 
-    // Check Z value
-    if (sim_datapath_out == 0) begin
-      assert (sim_Z_out == 1'b1) $display ("MVN sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end else begin
-      assert (sim_Z_out == 1'b0) $display ("MVN sucesssful");
-      else begin 
-        $display("Status incorrect, got %b but expected %b", sim_Z_out, expectedZ);
-        err = 1'b1;
-      end
-    end 
+    // Check status value
+    assert (sim_status_out == expectedStatus) $display ("SHIFT sucesssful");
+    else begin 
+      $display("Status incorrect, got %b but expected %b", sim_status_out, expectedStatus);
+      err = 1'b1;
+    end
     
-    MOV(Dest, sim_datapath_out);
+    MOV(Dest, sim_c, sim_c, `VSEL_C);
     #4;
     
   endtask
-
-  datapath DUT (.clk         (clk),
-
-              // register operand fetch stage
-              .readnum     (sim_readnum),
-              .vsel        (sim_vsel),
-              .loada       (sim_loada),
-              .loadb       (sim_loadb),
-
-              // computation stage (sometimes called "execute")
-              .shift       (sim_shift),
-              .asel        (sim_asel),
-              .bsel        (sim_bsel),
-              .ALUop       (sim_ALUop),
-              .loadc       (sim_loadc),
-              .loads       (sim_loads),
-
-              // set when "writing back" to register file
-              .writenum    (sim_writenum),
-              .write       (sim_write),  
-              .datapath_in (sim_datapath_in),
-
-              // outputs
-              .Z_out       (sim_Z_out),
-              .datapath_out(sim_datapath_out));
 
   initial forever begin
     clk = 1'b0; #2;
@@ -398,33 +383,84 @@ assign allRegs = {DUT.REGFILE.R7,
   // AND R4 R0 R1 // 0000 0000 0011 0010 & 0000 0000 0001 0101 = 0000 0000 0001 0000 
   // LSR R4 <> R2 1 // R4 = 0000 0000 0000 1010 = 10
   // ADD R5 R2 R3 LSL 1 // R5 = R2 + 2R3 = 71 + 58 = 129 
-  // SUB R6 R2 R4 LSR 1 = 71 - 5 = 66
-  // SUB R6 R6 R6 LSL 1  = -66
+  // SUB R6 R2 R4 LSR 1 = 71 - 35/2 = 54
+  // SUB R6 R6 R6 LSL 1  = -54
   // SUB R6 R6 R6 = 0 (Z = 1)
   // MOV R7 6
   // MVN R7 R7 <>;
-  // ADD R7 R7 #1;
+  // ADD R7 R7 #1
   // ADD R7 R7 #25 // R7 =  19
+
+  // OVERFLOW TEST
+  //
+  //
  
 
-  MOV(`r0, 50);
-  MOV (`r1, 21);
-  AritOP(`ADD, `r2, `r0, `r1, `NO_SHIFT, 71, 0);
-  AritOP(`SUB, `r3, `r0, `r1, `NO_SHIFT, 29, 0);
-  AritOP(`AND, `r4, `r0, `r1, `NO_SHIFT, 16'b0000_00000_0001_0000, 0);
-  LS(`R_1, `r4, `r2, 35, 0);
-  AritOP(`ADD, `r5, `r2, `r3, `L_1, 129, 0);
-  AritOP(`SUB, `r6, `r2, `r4, `R_1, 54, 0);
-  AritOP(`SUB, `r6, `r6, `r6, `L_1, -54, 0);
-  AritOP(`SUB, `r6, `r6, `r6, `NO_SHIFT, 0, 1);
-  MOV(`r7, 6);
-  MVN(`r7, `r7, `NO_SHIFT, 16'b1111_1111_1111_1001, 0);
-  OP_I(`ADD, `r7, `r7, 1, `NO_SHIFT, -6, 0);
-  OP_I(`ADD, `r7, `r7, 25, `NO_SHIFT, 19, 0);
+  MOV(`r0, 50, 50, `VSEL_IMM);
+  MOV (`r1, 21, 21,`VSEL_IMM);
+  AritOP(`ADD, `r2, `r0, `r1, `NO_SHIFT, 71, 3'b000);
+  AritOP(`SUB, `r3, `r0, `r1, `NO_SHIFT, 29, 3'b000);
+  AritOP(`AND, `r4, `r0, `r1, `NO_SHIFT, 16'b0000_00000_0001_0000, 3'bxxx);
+  LS(`R_1, `r4, `r2, 35, 3'b000);
+  AritOP(`ADD, `r5, `r2, `r3, `L_1, 129, 3'b000);
+  AritOP(`SUB, `r6, `r2, `r4, `R_1, 54, 3'b000);
+  AritOP(`SUB, `r6, `r6, `r6, `L_1, -54, 3'b001);
+  AritOP(`SUB, `r6, `r6, `r6, `NO_SHIFT, 0, 3'b100);
+
+  MOV(`r7, 6, 6, `VSEL_IMM);
+  MVN(`r7, `r7, `NO_SHIFT, 16'b1111_1111_1111_1001, 3'b001);
+  OP_I(`ADD, `r7, `r7, 1, `NO_SHIFT, -6, 3'b001);
+  OP_I(`ADD, `r7, `r7, 10, `NO_SHIFT, 4, 3'b000);  
+
+  // TEST LOADA/LOADB
+  // readA, readB, but load a and load b = 0, then c should be the same 
+
+  sim_readnum = `r0;
+  sim_readnum = `r1;
+  
+  #4
+  
+  sim_loadc = 1'b1;
+
+  #4
+
+  sim_loadc = 1'b0;
+
+  assert (sim_c == 4) $display ("value did not change");
+    else begin 
+      $display("ERROR value changed");
+      err = 1'b1;
+  end
+
+  // TEST LOADC
+  // loadA, loadB, but dont lpoad c, c should be the same.
+  sim_readnum = `r1;
+  sim_readnum = `r2;
+
+
+  // loads A and B, but not C
+  sim_loada = 1'b1;
+  sim_loadb = 1'b1;
+  sim_loadc = 1'b0;
+
+  #4 
+
+  sim_loada = 1'b0;
+  sim_loadb = 1'b0;
+
+  #4
+
+  // output should not change
+  
+  assert (sim_c == 4) $display ("value did not change");
+    else begin 
+      $display("ERROR value changed");
+      err = 1'b1;
+  end
 
   assert (err == 0) $display ("Passed all tests");
   else $display ("FAILED");
-  #220;
+  #208;
   $stop;
 end  
 
